a637a642a8deb07ee1955dfe93ac268cf6162821
Lexarque <satrialdyfarizki06@gmail.com>
2022-11-09 23:06:17 +0700

    Add permission list config v1

---
+++ config/permission-list.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'full_access_permission' => [
+        'full-access'
+    ],
+
+    'student_class_permission' => [
+        'view-student-classes',
+        'create-student-class',
+        'update-student-class',
+        'delete-student-class'
+    ],
+
+    'student_permission' => [
+        'view-students-data',
+        'show-student-data',
+        'create-student-data',
+        'update-student-data',
+        'delete-student-data'
+    ],
+
+    'tuition_permission' => [
+        'view-tuitions-data',
+        'show-tuition-data',
+        'create-tuition-data',
+        'update-tuition-data',
+        'delete-tuition-data'
+    ],
+
+    'payment_permission' => [
+        'view-payments-data',
+        'update-payment-data',
+        'view-payment-summary',
+    ],
+
+];
