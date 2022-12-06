667060345fd61f67be44586bd99f779289365286
Lexarque <satrialdyfarizki06@gmail.com>
2022-12-06 09:55:37 +0700

    Added role in user migration file

--- database/migrations/2022_08_09_000000_create_users_table.php
+++ database/migrations/2022_08_09_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -18,14 +19,20 @@ return new class extends Migration
             $table->string('nisn', 10)->unique()->nullable();
             $table->string('nis', 8)->unique()->nullable();
             $table->string('name', 35);
+            $table->string('address', 225);
+            $table->string('telephone_number', 20);
             $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 25);
-            $table->foreignId('student_class_id')->constrained('student_class');
+            $table->foreignId('student_class_id')->nullable()->constrained('student_class');
             $table->foreignId('role_id')->constrained('roles');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'student']);
+        Role::create(['name' => 'staff']);
     }
 
     /**
@@ -35,6 +42,8 @@ return new class extends Migration
      */
     public function down()
     {
+        Role::whereIn('name', ['student', 'staff', 'admin'])->delete();
+
         Schema::dropIfExists('users');
     }
 };
